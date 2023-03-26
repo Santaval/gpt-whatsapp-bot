a2883c22f41d08ff7ef46617af2e09e53d3ef54f
aaronsv <santanavaldelomaraa@gmail.com>
2023-03-26 13:24:59 -0600

    json fix

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
-const qrcode = require('qrcode-terminal')
-const {Client, LocalAuth} = require('whatsapp-web.js')
-const fs = require("fs")
-
-
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const fs = require("fs");
 
 const client = new Client({
-    authStrategy: new LocalAuth()
-})
+  authStrategy: new LocalAuth(),
+});
 
-client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true})
-})
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
 
+client.on("ready", () => {
+  console.log("Ciient ready");
+});
 
+client.on("message", (message) => {
+  client.sendMessage(message.from, "Hola, esto es un bot");
+});
 
-
-client.on('ready', () => {
-    console.log('Ciient ready')
-    client.sendMessage("50687156553@c.us", "Hola Aaron")
-        .then(res => {
-            if (res.id.fromMe) {
-                console.log('Enviado')
-            }
-        })
-})
-
-client.initialize()
-
+client.initialize();
